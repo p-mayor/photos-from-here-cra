85d355472879aa08cdd78665c6a57f6a8d55a8a5
tamburkey <petem14@gmail.com>
2020-08-12 12:08:07 -0400

    prevent currentPhotoNum from going above 4

--- src/components/display/Display.jsx
+++ src/components/display/Display.jsx
@@ -26,7 +26,7 @@ class Display extends React.Component {
     getPictures() {
         let requestURL = `https://shrouded-mountain-15003.herokuapp.com/https://flickr.com/services/rest/?api_key=312e305e7062fdb7eb699961353a06bd&format=json&nojsoncallback=1&method=flickr.photos.search&safe_search=1&per_page=5&lat=${this.state.lat}&lon=${this.state.lon}&text=dog`
 
-        fetch(requestURL).then((response)=> {
+        fetch(requestURL).then((response) => {
             return response.json();
         }).then((json) => {
             this.setState({ photos: json.photos.photo })
@@ -42,9 +42,11 @@ class Display extends React.Component {
     }
 
     clickHandler = (e) => {
-        this.setState((prevState) => {
-            return { currentPhotoNum: prevState.currentPhotoNum + 1 }
-        })
+        if (this.state.currentPhotoNum < 4) {
+            this.setState((prevState) => {
+                return { currentPhotoNum: prevState.currentPhotoNum + 1 }
+            })
+        }
     }
 
     render() {
@@ -52,7 +54,7 @@ class Display extends React.Component {
         return (
             <div className="Display">
                 <div>
-                {this.state.photos.length && <img src={this.constructImageURL(currentPhoto)} alt={currentPhoto.title} />}
+                    {this.state.photos.length && <img src={this.constructImageURL(currentPhoto)} alt={currentPhoto.title} />}
                 </div>
                 <div>
                     Latitude: {this.state.lat} Longitude: {this.state.lon}
