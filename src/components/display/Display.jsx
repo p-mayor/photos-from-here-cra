59879ca2deb85de5cba070a928a7c0868d5cd481
tamburkey <petem14@gmail.com>
2020-08-12 21:42:11 -0400

    refactor async/await

--- src/components/display/Display.jsx
+++ src/components/display/Display.jsx
@@ -12,7 +12,7 @@ class Display extends React.Component {
     }
 
     componentDidMount() {
-        let geo = navigator.geolocation
+        const geo = navigator.geolocation
 
         geo.getCurrentPosition((loc) => {
             this.setState({ lat: loc.coords.latitude, lon: loc.coords.longitude })
@@ -23,16 +23,18 @@ class Display extends React.Component {
         })
     }
 
-    getPictures() {
-        let requestURL = `https://shrouded-mountain-15003.herokuapp.com/https://flickr.com/services/rest/?api_key=312e305e7062fdb7eb699961353a06bd&format=json&nojsoncallback=1&method=flickr.photos.search&safe_search=1&per_page=5&lat=${this.state.lat}&lon=${this.state.lon}&text=dog`
+    async getPictures() {
+        const requestURL = `https://shrouded-mountain-15003.herokuapp.com/https://flickr.com/services/rest/?api_key=312e305e7062fdb7eb699961353a06bd&format=json&nojsoncallback=1&method=flickr.photos.search&safe_search=1&per_page=5&lat=${this.state.lat}&lon=${this.state.lon}&text=dog`
 
-        fetch(requestURL).then((response) => {
-            return response.json();
-        }).then((json) => {
-            this.setState({ photos: json.photos.photo })
-        }).catch((err) => {
-            console.log('Fetch problem: ' + err.message);
-        });
+        let response
+
+        try {
+            response = await (await fetch(requestURL)).json()
+            console.log(response)
+            this.setState({ photos: response.photos.photo })
+        } catch (err) {
+            console.log(err.message)
+        }
     }
 
     constructImageURL(photoObj) {
@@ -47,7 +49,7 @@ class Display extends React.Component {
                 return { currentPhotoNum: prevState.currentPhotoNum + 1 }
             })
         } else {
-            this.setState({currentPhotoNum:0})
+            this.setState({ currentPhotoNum: 0 })
         }
     }
 
@@ -57,7 +59,7 @@ class Display extends React.Component {
             <div className="Display">
                 <button onClick={this.clickHandler}>Next</button>
                 <div>
-                    {this.state.photos.length && <img src={this.constructImageURL(currentPhoto)} alt={currentPhoto.title} />}
+                    {this.state.photos.length > 0 && <img src={this.constructImageURL(currentPhoto)} alt={currentPhoto.title} />}
                 </div>
                 <div>
                     Latitude: {this.state.lat} Longitude: {this.state.lon}
